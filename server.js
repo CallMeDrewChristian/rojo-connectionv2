4d2d4eeca6d4c2baf619c610496792b2cf5f30fa
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2022-09-19 13:12:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 console.log("Up and Ready");
 
-const {exec} = require("child_process");
+const {
+    exec
+} = require("child_process");
 
 const express = require("express");
 
+const fs = require("fs");
+
 const app = express();
+
+const rojo_url = "https://github.com/CallMeDrewChristian/rojo-data/raw/main/rojo.exe";
+
+const http_options = {
+    method: 'GET',
+    url: `${rojo_url}`,
+};
 
 app.use(express.static("public"));
 
 app.get("/", async (request, response) => {
-  response.send("Test");
-});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+                request(http_options, function(error, response, body) {
+                    if (error) throw new Error(error);
+                    const fileStream = fs.createWriteStream('rojo.exe');
+                    fileStream.on("error", function(err) {
+                        console.log("Error writing to the stream");
+                        console.log(err);
+                    }; fileStream.on("finish", function() {
+                            exec("rojo serve", (error, stdout, stderr) => {
+                                if (error) {
+                                    console.log(`error: ${error.message}`);
+                                    return;
+                                };
+                                if (stderr) {
+                                    console.log(`stderr: ${stderr}`);
+                                    return;
+                                };
+                                console.log(`stdout: ${stdout}`);
+                            });
+                        };
+                    });
+                });
+
+                const listener = app.listen(process.env.PORT || 3000, () => {
+                    console.log("Your app is listening on port " + listener.address().port);
+                });
